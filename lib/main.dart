02cb884488fd871f97ad23b0a96f163c6d12ef44
abrilskop <uberpot.ia@gmail.com>
2025-03-25 15:38:51 -0500

    feat: add actividad 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:sqflite/sqflite.dart';
-
+import 'package:test_sqlite/libros.dart';
+import 'package:test_sqlite/database_helper.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,116 +9,178 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'CRUD Libros',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final DatabaseHelper _dbHelper = DatabaseHelper();
+  final TextEditingController _EditTituloLibro=TextEditingController();
+  List<Libro> _items = [];
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _cargarListaLibros();
+  }
+
+  Future<void> _cargarListaLibros() async {
+    final items = await _dbHelper.getItems();
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _items = items;
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+  void _agregarNuevoLibro(String tituloLibro) async {
+    final nuevoLibro = Libro(tituloLibro: tituloLibro);
+    await _dbHelper.insertLibro(nuevoLibro);
+    print("NUEVO LIBRO AGREGADO CON EXITO");
+    _cargarListaLibros();
+  }
+
+  void _mostrarVentanaAgregar(){
+    showDialog(
+      context: context,
+      builder: (context){
+        return AlertDialog(
+          title: const Text("Agregar Titulo"),
+          content: TextField(
+            controller: _EditTituloLibro,
+            decoration: const InputDecoration(hintText: "Ingrese el titulo"),
+          ),
+          actions: [
+            TextButton(
+              onPressed: (){
+                if(_EditTituloLibro.text!.isNotEmpty){
+                  _agregarNuevoLibro(_EditTituloLibro.text.toString());
+                  Navigator.of(context).pop();
+                }
+              },
+              child: Text("Agregar"),
             ),
           ],
-        ),
+        );
+      } 
+    );
+  }
+
+  void _eliminarLibro(int id) async {
+    await _dbHelper.eliminar('libros', where: 'id = ?', whereArgs: [id]);
+    _cargarListaLibros();
+  }
+
+  void _mostrarMensajeModificar(int id){
+    showDialog(
+      context: context,
+      builder: (context){
+        return AlertDialog(
+          title: Text("Confirmar eliminacion"),
+          content: Text("Estas seguro de que quieres eliinar este libro?"),
+          actions: [
+            TextButton(
+              onPressed: (){
+                Navigator.of(context).pop();
+              },
+              child: Text("Cancelar"),
+            ),
+          ],
+        );
+      }
+    );
+  }
+
+  void _actualizarLibro(int id, String nuevoTitulo) async {
+    await _dbHelper.actualizar(
+      'libros',
+      {'tituloLibro': nuevoTitulo},
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+    _cargarListaLibros();
+  }
+
+  void _ventanaEditar(int id, String tituloActual) {
+    TextEditingController _tituloController = TextEditingController(text: tituloActual);
+
+    showDialog(
+      context: context,
+      builder: (context){
+        return AlertDialog(
+          title: Text("Modificar Titulo del Libro"),
+          content: TextField(
+            controller: _tituloController,
+            decoration: InputDecoration(
+              hintText: "Escribe el nuevo titulo"
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: (){
+                Navigator.of(context).pop();
+              },
+              child: Text("Cancelar"),
+            ),
+            TextButton(
+              onPressed: (){
+                if (_tituloController.text!.isNotEmpty){
+                  _actualizarLibro(id, _tituloController.text.toString());
+                }
+                Navigator.of(context).pop();
+              },
+              child: Text("Guardar"),
+            ),
+          ],
+        );
+      }
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Gestión de Libros'),
+        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+      ),
+      body: ListView.separated(
+        itemCount: _items.length,
+        separatorBuilder: (context, index) => Divider(),
+        itemBuilder: (context, index) {
+          final libro = _items[index];
+          return ListTile(
+            title: Text(libro.tituloLibro),
+            subtitle: Text('ID: ${libro.id}'),
+            trailing: IconButton(
+              icon: Icon(Icons.delete, color: Colors.grey),
+              onPressed: () {
+                _mostrarMensajeModificar(int.parse(libro.id.toString()));
+              },
+            ),
+            onTap: (){
+              _ventanaEditar(int.parse(libro.id.toString()), libro.tituloLibro);
+            },
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () => _mostrarVentanaAgregar(),
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
